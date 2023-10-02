194d7a3abd21e886006e49e805c1ff94e4df7bf4
Felipe Badia <137180694+felipebdia@users.noreply.github.com>
2023-10-02 09:15:52 -0300

    Update ex011.js

--- aulas/aula12/ex011.js
+++ aulas/aula12/ex011.js
@@ -1,6 +1,6 @@
-var idade = 12
+var idade = 16
 if (idade < 18) {
-    console.log("Menor de idade")
+    console.log("Não vota")
 } else {
-    console.log("Maior de idade")
+    console.log("Vota")
 }
