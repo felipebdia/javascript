572469499551dd2920e9ff9494b806a6b646bc3f
Felipe Badia <137180694+felipebdia@users.noreply.github.com>
2023-10-11 18:51:09 -0300

    att

--- aulas/aula13/ambiente0.js
+++ aulas/aula13/ambiente0.js
@@ -1,6 +1,15 @@
+/*
 console.log('Tudo bem')
 console.log('Tudo bem')
 console.log('Tudo bem')
 console.log('Tudo bem')
 console.log('Tudo bem')
 console.log('Tudo bem')
+*/
+
+var c = 1
+
+while (c <= 50) {
+    console.log(`Passo ${c}`)
+    c++
+}
